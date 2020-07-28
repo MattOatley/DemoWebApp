57ea0bf30541994ed1c18249feae237f86c61edf
Matt Oatley <Matthew.Oatley225@gmail.com>
2020-07-28 15:20:34 -0500

    Finished exercise, created working products tab

---
+++ DemoWebProject/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoWebProject.Models;
+
+namespace DemoWebProject
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
